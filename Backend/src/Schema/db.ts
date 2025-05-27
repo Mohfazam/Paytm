9399703deec8bdfc5a27ce109eb118889c3b8e6e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-27 15:04:53 +0530

    SCHEMA

---
+++ Backend/src/Schema/db.ts
@@ -0,0 +1,12 @@
+import  mongoose  from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: {required: true, type: String, unique: true, trim:true, lowercase:true, minLength: 3, maxLength: 30},
+    password: {required: true, type: String, minLength: 3},
+    firstname: {required: true, type: String, trim: true, maxLength: 50},
+    lastname: {required: true, type: String, trim: true, maxLength: 50},
+    
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
