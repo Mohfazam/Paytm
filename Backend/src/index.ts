8605bd7b0dd99e1bc2ddc39ae7bcf81fab4d562b
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-27 15:32:04 +0530

    Router

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,2 +1,11 @@
 import express from "express";
 import {User} from './Schema/db'
+
+import { router as MainRouter } from './Routes/index'
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", MainRouter);
+
