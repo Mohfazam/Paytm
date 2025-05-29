95f9a45f3d45cd317b8b01da49b64c1400075c99
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-30 01:17:52 +0530

    Basic Backend

--- Backend/src/Routes/UserRoute/index.ts
+++ Backend/src/Routes/UserRoute/index.ts
@@ -1,10 +1,25 @@
 import express from 'express';
+import {z} from 'zod/v4';
+import { User } from '../../Schema/db';
 
 export const userRouter = express.Router();
 
 userRouter.get("/Health", (req, res) => {
+
+    const UserInput = z.object({
+        username: z.string().trim().lowercase(),
+        password: z.string().min(3),
+        firstname: z.string().max(50),
+        lastname: z.string().max(50)
+
+    })
+
     res.status(200).json({
-        msg: "WORKED"
+        msg: " User EndPoint is Running Up and Workind"
     });
 });
 
+
+userRouter.post("/Signup", (req, res) => {
+    const {username, password, firstname, lastname} = req.body;
+});
