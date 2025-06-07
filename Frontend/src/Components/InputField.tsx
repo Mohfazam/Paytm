d2a1a39959b18560479bf58b16ef6dc80b7a7c8f
Mohfazam <mohd.sarwar.code@gmail.com>
2025-06-07 14:07:53 +0530

    SubHeading.tsx

---
+++ Frontend/src/Components/InputField.tsx
@@ -0,0 +1,5 @@
+export const InputField = () => {
+    return <div>
+        
+    </div>
+}
--- Frontend/src/Test.tsx
+++ Frontend/src/Test.tsx
@@ -1,9 +1,11 @@
 import { Heading } from "./Components/Heading"
+import { InputField } from "./Components/InputField"
 import { SubHeading } from "./Components/Subheading"
 
 export const Test = () => {
     return <div>
         <Heading label={"Hello"}/>
         <SubHeading Text={"Hello again but its an subheading so it should be long"} />
+        <InputField />
     </div>
 }
