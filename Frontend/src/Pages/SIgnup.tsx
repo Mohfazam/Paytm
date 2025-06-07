7b133574c10b22c142c658d1b2b1f466fae65b22
Mohfazam <mohd.sarwar.code@gmail.com>
2025-06-07 14:27:15 +0530

    Restructuing

--- Frontend/src/Pages/SIgnup.tsx
+++ Frontend/src/Pages/SIgnup.tsx
@@ -1,7 +1,15 @@
+import { BottomWarning } from "../Components/BottomWarning"
+import { Button } from "../Components/Button"
+import { Heading } from "../Components/Heading"
+import { InputField } from "../Components/InputField"
+import { SubHeading } from "../Components/Subheading"
+
 export const Signup = () => {
-    return(
-    <div>
-        help Signup
+    return <div>
+        <Heading Label={"Hello"}/>
+        <SubHeading Text={"Hello again but its an subheading so it should be long"} />
+        <InputField Label="First Name" PlaceHolder={"Enter Your Name"}/>
+        <Button Label="Click Me"/>
+        <BottomWarning Label={"Don't Have an Account?"} ButtonText={"Sign Up"} to={"/ToUnderline"}/>
     </div>
-    )
 }
