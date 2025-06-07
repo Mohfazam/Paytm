66304a098cfd635319efebd6f91754074258fb32
Mohfazam <mohd.sarwar.code@gmail.com>
2025-06-07 14:37:35 +0530

    SigIn

--- Frontend/src/Pages/Signin.tsx
+++ Frontend/src/Pages/Signin.tsx
@@ -8,12 +8,12 @@ export const SignIn = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-        <Heading Label={"Sign In"} />
+        <Heading Label={"SignIn"} />
         <SubHeading Label={"Enter your credentials to access your account"} />
         <InputField PlaceHolder="example@gmail.com" Label={"Email"} />
         <InputField PlaceHolder="123456" Label={"Password"} />
         <div className="pt-4">
-          <Button Label={"Sign up"} />
+          <Button Label={"Signin"} />
         </div>
         <BottomWarning Label={"Don't Have an Account?"} ButtonText={"Create one"} to={"/signin"} />
       </div>
